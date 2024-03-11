898f89667f9c8aebff6acb1cc58412475e912538
AlifBrahim <60931934+AlifBrahim@users.noreply.github.com>
2024-03-11 10:11:38 +0800

    Initial commit

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import digest
+from . import ir_module
+from . import helpdesk
+from . import helpdesk_ticket
+from . import mail_template
+from . import res_users
+from . import res_partner
